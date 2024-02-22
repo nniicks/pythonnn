228084811947fa572af9983a453e05320f482e94
nniicks <jhonathanduartebarreto@gmail.com>
2024-02-22 15:02:35 -0400

    fisio_3d

---
+++ fisio_3d/correlacao.py
@@ -0,0 +1,13 @@
+def calcular_correlao(lista_1, lista_2):
+    correlacao = 0
+
+
+
+    return correlacao
+
+
+dados_1 = [2.3, 1, 0.75, 3, 1.75]
+dados_2 = [0.33, 0.98, 1.23, 1.98, 2]
+
+resultado = calcular_correlao(dados_1, dados_2)
+print(f"A correlação entre os dados é: {resultado}")
